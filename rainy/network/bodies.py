ffd9f4307a0ceb4a2def7e328b4641255849080a
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-06-23 23:15:16 +0900

    Add NatureDqnBody

--- rainy/network/bodies.py
+++ rainy/network/bodies.py
@@ -1,29 +1,37 @@
 # network bodies
 from abc import ABC, abstractmethod
-from enum import Enum
 from torch import nn, Tensor
 import torch.nn.functional as F
+from typing import Optional
+
+from .init import Initializer
 
 class NetworkBody(nn.Module, ABC):
     @abstractmethod
     def feature_dim(self) -> int:
         pass
 
-
-class NatureDQNConv(NetworkBody):
-    """Convolutuion Network in https://www.nature.com/articles/nature14236
+class NatureDqnBody(NetworkBody):
+    """Convolutuion Network used in https://www.nature.com/articles/nature14236
     """
-    def __init__(self, input_channels:int = 4) -> None:
+    def __init__(self, ini: Initializer, input_channels:int = 4) -> None:
         super(NatureDQNConv, self).__init__()
         self.__feature_dim = 512
         conv1 = nn.Conv2d(input_channels, 32, kernel_size=8, stride=4)
         conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-
-        self.fc4 = nn.Linear(7 * 7 * 64, self.__feature_dim)
-
+        self.conv = ini.make_mod_list([conv1, conv2, conv3])
+        self.fc = nn.Linear(7 * 7 * 64, self.__feature_dim)
+        
     def feature_dim(self) -> int:
         return self.__feature_dim
 
     def forward(self, x: Tensor) -> Tensor:
-        x = F.relu(self.conv1(x))
+        for conv in self.conv:
+            x = F.relu(conv(x))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc(x))
+        return x
+
+
+
--- rainy/network/init.py
+++ rainy/network/init.py
@@ -1,20 +1,20 @@
 from functools import partial
-from typing import Callable
+from typing import Callable, List
 from torch import nn, Tensor
 
 InitFn = Callable[[Tensor], None]
 
-def uniform(mean: float = 0, var: float = 1) -> InitFn:
+def uniform(mean: float = 0, var: float = 1) -> int:
     return partial(nn.init.uniform_, a=mean, b=var)
 
-def orthiganl(gain: float = 1) -> InitFn:
+def orthogonal(gain: float = 1) -> InitFn:
     return partial(nn.init.orthogonal_, gain=gain)
-
-def zero() -> InitFn:
-    return partial(nn.init.constant_, val=0)
 
 def constant(val: float) -> InitFn:
     return partial(nn.init.constant_, val=val)
+
+def zero() -> InitFn:
+    return partial(nn.init.constant_, val=0)
 
 class Initializer:
     """Utility Class to initialize weight parameters of NN
@@ -24,11 +24,20 @@ class Initializer:
         self.bias_init = bias_init
         self.scale = scale
 
-    def __call__(self, mod: nn.Module) -> None:
+    def init(self, mod: nn.Module) -> None:
         self.weight_init(mod.weight.data)
         self.bias_init(mod.bias.data)
         mod.weight.data.mul_(self.scale)
+        
+    def init_list(self, mods: nn.ModuleList) -> None:
+        for mod in mods:
+            self.init(mod)
 
+    def make_mod_list(self, mods: List[nn.Module]) -> nn.ModuleList:
+        for mod in mods:
+            self.init(mod)
+        return nn.ModuleList(mods)
+                
 
 
 
