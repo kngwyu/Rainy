9178f601cb844db879cfa4597fb1bd81bf6b77eb
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-09-16 18:00:30 +0900

    remove env

--- rainy/config.py
+++ rainy/config.py
@@ -1,5 +1,3 @@
-import gym
-from gym import Env
 from numpy import ndarray
 from torch import Tensor
 from torch.optim import Optimizer, RMSprop
@@ -12,29 +10,25 @@ from .util import Device, loss
 
 class Config:
     def __init__(self) -> None:
-        # for value based methods
+        # initialized lazily
         self.action_dim = 0
+        self.state_dim = 0
+
         self.batch_size = 100
         self.discount_factor = 0.01
         self.device = Device()
         self.grad_clip = 5.0
         self.replay_size = 1000
-        self.state_dim = 0
         self.seed = 0
         self.train_start = 1000
-        self.__env = lambda: gym.make('CartPole-v0')
+
+        # for value based methods
         self.__exp = lambda net: EpsGreedy(0.01, LinearCooler(0.1, 0, 100000), net)
         self.__loss_function = loss.mean_squared_loss
         self.__optim = lambda params: RMSprop(params, 0.001)
         self.__replay = lambda capacity: UniformReplayBuffer(capacity)
         self.__vn = nature_dqn
         self.__wrap_states = lambda states: states
-
-    def env(self) -> Env:
-        return self.__env()
-
-    def set_env(self, env: Callable[[], Env]) -> None:
-        self.__env = env
 
     def explorer(self, value_net: ValueNet) -> Explorer:
         return self.__exp(value_net)
