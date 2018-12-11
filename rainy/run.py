a85e24ac16916eb654fccbb3513ac74b325c4d16
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-12-11 17:14:32 +0900

    Simplify logging code

--- rainy/run.py
+++ rainy/run.py
@@ -8,13 +8,6 @@ SAVE_FILE_DEFAULT = 'rainy-agent.save'
 ACTION_FILE_DEFAULT = 'actions.json'
 
 
-def __interval(turn: int, width: int, freq: Optional[int]) -> bool:
-    if freq:
-        return turn != 0 and turn // freq != (turn - width) // freq
-    else:
-        return False
-
-
 def train_agent(
         ag: Agent,
         save_file_name: str = SAVE_FILE_DEFAULT,
@@ -25,39 +18,55 @@ def train_agent(
     rewards = []
     end = False
     action_file = Path(action_file_name)
+
+    def log_episode(episodes: int, rewards: np.ndarray) -> None:
+        ag.logger.exp('train_reward', {
+            'episodes': episodes,
+            'total_steps': ag.total_steps,
+            'reward_mean': float(np.mean(rewards)),
+            'reward_max': float(np.max(rewards)),
+            'reward_min': float(np.min(rewards)),
+        })
+        rewards = []
+
+    def log_eval(episodes: int):
+        log_dir = ag.logger.log_dir
+        if ag.config.save_eval_actions and log_dir:
+            fname = log_dir.joinpath('{}-{}{}'.format(
+                action_file.stem,
+                episodes,
+                action_file.suffix
+            ))
+            reward = ag.eval_and_save(fname.as_posix())
+        else:
+            reward = ag.eval_episode()
+            ag.logger.exp('eval', {
+                'episodes': episodes,
+                'total_steps': ag.total_steps,
+                'reward': reward,
+            })
+
+    def interval(turn: int, width: int, freq: Optional[int]) -> bool:
+        if freq:
+            return turn != 0 and turn // freq != (turn - width) // freq
+        else:
+            return False
+
+    def truncate_episode(episodes: int, freq: int) -> int:
+        return episodes - episodes % freq
+
     while not end:
         if max_steps and ag.total_steps > max_steps:
             end = True
         tmp = ag.train_episode()
         episodes += len(tmp)
         rewards += tmp
-        if __interval(episodes, len(tmp),ag.config.episode_log_freq):
-            rewards = np.array(rewards)
-            ag.logger.exp('train_reward', {
-                'episodes': episodes,
-                'total_steps': ag.total_steps,
-                'reward_mean': float(np.mean(rewards)),
-                'reward_max': float(np.max(rewards)),
-                'reward_min': float(np.min(rewards)),
-            })
-            rewards = []
-        if end or __interval(episodes, len(tmp), ag.config.eval_freq):
-            log_dir = ag.logger.log_dir
-            if ag.config.save_eval_actions and log_dir:
-                fname = log_dir.joinpath('{}-{}{}'.format(
-                    action_file.stem,
-                    episodes,
-                    action_file.suffix
-                ))
-                reward = ag.eval_and_save(fname.as_posix())
-            else:
-                reward = ag.eval_episode()
-            ag.logger.exp('eval', {
-                'episodes': episodes,
-                'total_steps': ag.total_steps,
-                'reward': reward,
-            })
-        if end or __interval(episodes, len(tmp), ag.config.save_freq):
+        if interval(episodes, len(tmp), ag.config.episode_log_freq):
+            tmp_eps = truncate_episode(episodes, ag.config.episode_log_freq)
+            log_episode(tmp_eps, np.array(rewards[:tmp_eps]))
+        if end or interval(episodes, len(tmp), ag.config.eval_freq):
+            log_eval(truncate_episode(episodes, ag.config.eval_freq))
+        if end or interval(episodes, len(tmp), ag.config.save_freq):
             ag.save(save_file_name)
     ag.close()
 
@@ -74,5 +83,6 @@ def eval_agent(
         res = ag.eval_and_save(path.joinpath(action_file).as_posix())
     else:
         res = ag.eval_episode()
+    ag.close()
     print('reward: {}'.format(res))
 
