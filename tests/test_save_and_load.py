51c9622340e529ed4a33d087efa473b2f22eadb8
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-12-27 20:01:28 +0900

    Add test_save_and_load

---
+++ tests/test_save_and_load.py
@@ -0,0 +1,31 @@
+from rainy import Config
+from rainy.agent import PpoAgent
+from rainy.envs import MultiProcEnv
+from pathlib import Path
+from torch.optim import Adam
+LOG_DIR = Path('/tmp/rainy-test/')
+
+
+def config() -> Config:
+    c = Config()
+    c.max_steps = int(1e6)
+    c.nworkers = 8
+    c.set_parallel_env(lambda env_gen, num_w: MultiProcEnv(env_gen, num_w))
+    c.set_optimizer(lambda params: Adam(params, lr=2.5e-4, eps=1.0e-4))
+    c.ppo_clip = 0.5
+    if not LOG_DIR.exists():
+        LOG_DIR.mkdir()
+    c.logger.set_dir(LOG_DIR)
+    return c
+
+
+def test_ppo_save() -> None:
+    ppo = PpoAgent(config())
+    ppo.optimizer.param_groups[0]['lr'] = 1.0
+    ppo.clip_eps = 0.2
+    ppo.save('ppo-agent.save')
+    ppo = PpoAgent(config())
+    path = ppo.config.logger.log_dir.joinpath('ppo-agent.save')
+    ppo.load(path.as_posix())
+    assert ppo.clip_eps == 0.2
+    assert ppo.optimizer.param_groups[0]['lr'] == 1.0
