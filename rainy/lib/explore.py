b766a074ac557e73c60f83ee470ba6a069813c28
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-04-10 12:55:18 +0900

    Fix decay of lr and clip_eps

--- rainy/lib/explore.py
+++ rainy/lib/explore.py
@@ -7,13 +7,13 @@ from ..prelude import Array
 
 class Cooler(ABC):
     @abstractmethod
-    def __call__(self, eps: float) -> float:
+    def __call__(self) -> float:
         pass
 
     def lr_decay(self, optim: Optimizer) -> None:
         for param_group in optim.param_groups:
             if 'lr' in param_group:
-                param_group['lr'] = self.__call__(param_group['lr'])
+                param_group['lr'] = self.__call__()
 
 
 class LinearCooler(Cooler):
@@ -25,21 +25,24 @@ class LinearCooler(Cooler):
                 f'[LinearCooler.__init__] the minimal value({minimal})'
                 ' is bigger than the initial value {initial}'
             )
-        self.delta = (initial - minimal) / float(max_step)
+        self.base = initial - minimal
         self.minimal = minimal
+        self.max_step = max_step
+        self.left = max_step
 
-    def __call__(self, eps: float) -> float:
-        return max(eps - self.delta, self.minimal)
+    def __call__(self) -> float:
+        self.left = max(0, self.left - 1)
+        return (float(self.left) / self.max_step) * self.base + self.minimal
 
 
 class DummyCooler(Cooler):
     """Do nothing
     """
-    def __init__(self, *args) -> None:
-        pass
+    def __init__(self, initial: float, *args) -> None:
+        self.initial = initial
 
-    def __call__(self, eps: float) -> float:
-        return eps
+    def __call__(self) -> float:
+        return self.initial
 
 
 class Explorer(ABC):
@@ -64,7 +67,7 @@ class EpsGreedy(Explorer):
 
     def select_action(self, state: Array, value_pred: ValuePredictor) -> int:
         old_eps = self.epsilon
-        self.epsilon = self.cooler(self.epsilon)
+        self.epsilon = self.cooler()
         if np.random.rand() < old_eps:
             action_dim = value_pred.action_dim
             return np.random.randint(0, action_dim)
