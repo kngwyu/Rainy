e97af05317c27a4cf88bd3bd836c620ce216b577
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-05-16 11:51:36 +0900

    Expose RMSs in utils.__init__

--- rainy/utils/__init__.py
+++ rainy/utils/__init__.py
@@ -1,3 +1,5 @@
 from .device import Device
 from .log import Logger
+from .misc import iter_prod, normalize_
+from .rms import RunningMeanStd, RunningMeanStdTorch
 from .sample import sample_indices
