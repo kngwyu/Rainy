17f86bd3317f924f22438e6aba4afcbcb24253c7
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-05-16 16:56:10 +0900

    Expose calc_cnn_hidden

--- rainy/net/__init__.py
+++ rainy/net/__init__.py
@@ -1,5 +1,6 @@
 from .actor_critic import ActorCriticNet, SharedBodyACNet
 from .block import Activator, ConvBody, DqnConv, FcBody, ResNetBody, LinearHead, NetworkBlock
+from .block import calc_cnn_hidden
 from .init import InitFn, Initializer
 from .policy import Policy, PolicyHead
 from .recurrent import DummyRnn, GruBlock, LstmBlock, RnnBlock, RnnState
