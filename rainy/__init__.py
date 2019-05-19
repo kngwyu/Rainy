bc8e7ce4d2ed445db0d271ca1fd32259ac5aa24c
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-05-19 13:09:41 +0900

    Remove redundant imports

--- rainy/__init__.py
+++ rainy/__init__.py
@@ -1,11 +1,5 @@
 from .config import Config
 from .run import train_agent
-from . import agents
-from . import envs
-from . import lib
-from . import net
-from . import replay
-from . import utils
 
 
 __version__ = '0.1.0'
