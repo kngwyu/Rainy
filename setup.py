0eadc076b7d4f70e236eb05c8f23389fc8c8be89
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-05-26 21:51:09 +0900

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
 import io
 import re
-from setuptools import setup, find_packages
-import sys
+from setuptools import Command, find_packages, setup
+from shutil import rmtree
 
 
-py_major = sys.version_info.major
-py_minor = sys.version_info.minor
-if py_major != 3 or py_minor <= 5:
-    print('This package is only compatible with Python>=3.6, but you are running '
-          'Python {}.{}. The installation will likely fail.'.format(py_major, py_minor))
+NAME = 'rainy'
+AUTHOR = 'Yuji Kanagawa'
+EMAIL = 'yuji.kngw.80s.revive@gmail.com'
+REQUIRES_PYTHON = '>=3.6.0'
+DESCRIPTION = 'Algorithm and utilities for deep reinforcement learning'
 
-with io.open('rainy/__init__.py', 'rt', encoding='utf8') as f:
-    version = re.search(r"__version__ = \'(.*?)\'", f.read()).group(1)
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'rainy/__init__.py'), 'rt', encoding='utf8') as f:
+    VERSION = re.search(r"__version__ = \'(.*?)\'", f.read()).group(1)
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        LONG_DESCRIPTION = '\n' + f.read()
+except FileNotFoundError:
+    LONG_DESCRIPTION = DESCRIPTION
 
 
-description = '''
-Rainy
-=====
-Reinforcement learning utilities and algrithm implementations using PyTorch.
-
-Please see https://github.com/kngwyu/Rainy for detail.
-'''
-
-
-requirements = [
+REQUIRED = [
     'click>=7.0',
     'GitPython>=2.0',
     'gym[atari]>=0.11.0',
@@ -32,38 +31,77 @@ requirements = [
     'Pillow>=5.0',
     'torch>=1.0',
 ]
-test_requirements = ['pytest>=3.0']
-extra_requirements = {
+TEST = ['pytest>=3.0']
+EXTRA = {
     'ipython': ['ipython>=7.0', 'matplotlib>=3.0', 'ptpython>=2.0'],
     'bullet': ['pybullet>=2.4']
 }
 
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(VERSION))
+        os.system('git push --tags')
+        sys.exit()
+
+
 setup(
-    name='rainy',
-    version=version,
-    url='https://github.com/kngwyu/Rainy',
+    name=NAME,
+    version=VERSION,
+    url=URL,
     project_urls={
-        'Code': 'https://github.com/kngwyu/Rainy',
-        'Issue tracker': 'https://github.com/kngwyu/Rainy/issues',
+        'Code': URL,
+        'Issue tracker': URL + '/issues',
     },
-    author='Yuji Kanagawa',
-    author_email='yuji.kngw.80s.revive@gmail.com',
-    description='Algorithm and utilities for deep reinforcement learning',
-    long_description=description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    python_requires='>=3.6',
-    install_requires=requirements,
-    test_requires=test_requirements,
-    extras_require=extra_requirements,
+    python_requires=REQUIRES_PYTHON,
+    install_requires=REQUIRED,
+    test_requires=TEST,
+    extras_require=EXTRA,
+    license='Apache2',
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
     ],
 )
