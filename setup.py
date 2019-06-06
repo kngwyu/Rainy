e92f9d029fa2825f9beaa348bc7041ca6997667b
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-06-06 17:25:04 +0900

    Removes unused UploadCommand from setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 import io
 import os
 import re
-from setuptools import Command, find_packages, setup
-from shutil import rmtree
+from setuptools import find_packages, setup
 
 
 NAME = 'rainy'
@@ -38,42 +37,6 @@ EXTRA = {
     'ipython': ['ipython>=7.0', 'matplotlib>=3.0', 'ptpython>=2.0'],
     'bullet': ['pybullet>=2.4']
 }
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(VERSION))
-        os.system('git push --tags')
-        sys.exit()
 
 
 setup(
