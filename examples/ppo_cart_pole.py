e3cf4e30ab5374aa4cdd74f05c00b79da83b3bec
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-07-28 17:56:58 +0900

    Change default parameters for ppo_cart_pole

--- examples/ppo_cart_pole.py
+++ examples/ppo_cart_pole.py
@@ -13,13 +13,14 @@ def config() -> rainy.Config:
     c.set_parallel_env(MultiProcEnv)
     c.set_optimizer(lambda params: Adam(params, lr=2.5e-4, eps=1.0e-4))
     c.value_loss_weight = 0.2
+    c.entropy_weight = 0.001
     c.grad_clip = 0.1
     c.gae_lambda = 0.95
     c.ppo_minibatch_size = 64
     c.use_gae = True
     c.ppo_clip = 0.2
     c.eval_freq = None
-    c.set_net_fn('actor-critic', rainy.net.actor_critic.fc_shared(rnn=rainy.net.GruBlock))
+    # c.set_net_fn('actor-critic', rainy.net.actor_critic.fc_shared(rnn=rainy.net.GruBlock))
     return c
 
 
