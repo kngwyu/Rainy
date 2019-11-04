f2a1b8f9b8478d7debcc4bdf0bf218fcf8e6f3df
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-11-05 00:20:05 +0900

    Implement TanhGaussianPolicy

--- rainy/net/policy.py
+++ rainy/net/policy.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
+import torch
 from torch import nn, Tensor
 from torch.distributions import Bernoulli, Categorical, Distribution, Normal
 from torch.nn import functional as F
-from typing import Optional
+from typing import Optional, Tuple
 from ..prelude import Array, Index, Self
 from ..utils import Device
 
@@ -13,19 +14,36 @@ class Policy(ABC):
     def __init__(self, dist: Distribution) -> None:
         self.dist = dist
         self._action: Optional[Tensor] = None
+        self._baction: Optional[Tensor] = None
 
     def action(self) -> Tensor:
         """Sample actions if this policy has no action cache.
         """
         if self._action is None:
-            self._action = self.dist.sample().detach()
+            self._action = self.sample()
         return self._action.squeeze()
+
+    def baction(self) -> Tensor:
+        """Sample "backwardable" actions.
+        """
+        if self._baction is None:
+            self._baction = self.rsample()
+        return self._baction.squeeze()
 
     def action_as_np(self) -> Array:
         return self.action().cpu().numpy()
 
     def set_action(self, action: Tensor) -> None:
         self._action = action
+
+    def sample(self) -> Tensor:
+        return self.dist.sample().detach()
+
+    def rsample(self) -> Tensor:
+        """Sampling by reparameterization trick,
+        which means the returned tensor is backwardable
+        """
+        return self.dist.rsample()
 
     @abstractmethod
     def __getitem__(self, idx: Index) -> Self:
@@ -36,7 +54,7 @@ class Policy(ABC):
         pass
 
     @abstractmethod
-    def log_prob(self) -> Tensor:
+    def log_prob(self, use_baction: bool = False) -> Tensor:
         pass
 
     @abstractmethod
@@ -51,8 +69,11 @@ class BernoulliPolicy(Policy):
     def best_action(self) -> Tensor:
         return self.dist.probs > 0.5
 
-    def log_prob(self) -> Tensor:
-        return self.dist.log_prob(self.action())
+    def log_prob(self, use_baction: bool = False) -> Tensor:
+        if use_baction:
+            return self.dist.log_prob(self.baction())
+        else:
+            return self.dist.log_prob(self.action())
 
     def entropy(self) -> Tensor:
         return self.dist.entropy()
@@ -68,8 +89,11 @@ class CategoricalPolicy(Policy):
     def best_action(self) -> Tensor:
         return self.dist.probs.argmax(dim=-1)
 
-    def log_prob(self) -> Tensor:
-        return self.dist.log_prob(self.action())
+    def log_prob(self, use_baction: bool = False) -> Tensor:
+        if use_baction:
+            return self.dist.log_prob(self.baction())
+        else:
+            return self.dist.log_prob(self.action())
 
     def entropy(self) -> Tensor:
         return self.dist.entropy()
@@ -88,11 +112,44 @@ class GaussianPolicy(Policy):
     def entropy(self) -> Tensor:
         return self.dist.entropy().sum(-1)
 
-    def log_prob(self) -> Tensor:
-        return self.dist.log_prob(self.action()).sum(-1)
+    def log_prob(self, use_baction: bool = False) -> Tensor:
+        if use_baction:
+            return self.dist.log_prob(self.baction()).sum(-1)
+        else:
+            return self.dist.log_prob(self.action()).sum(-1)
 
     def __getitem__(self, idx: Index) -> Self:
         return GaussianPolicy(self.dist.mean[idx], self.dist.stddev[idx])
+
+
+class TanhGaussianPolicy(GaussianPolicy):
+    def __init__(self, *args, epsilon: float = 1e-6, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._pre_tanh = None
+        self.epsilon = epsilon
+
+    def sample(self) -> Tensor:
+        self._pre_tanh = self.dist.sample().detach()
+        return torch.tanh(self._pre_tanh)
+
+    def rsample(self) -> Tensor:
+        res = self.dist.rsample()
+        assert res.requires_grad
+        self._pre_tanh = res
+        return torch.tanh(res)
+
+    def best_action(self) -> Tensor:
+        return torch.tanh(self.dist.mean)
+
+    def log_prob(self, use_baction: bool = False) -> Tensor:
+        action = self._baction if use_baction else self._action
+        if self._pre_tanh is None:
+            pre_tanh = torch.log((1.0 + action) / (1.0 - action)) / 2
+        else:
+            pre_tanh = self._pre_tanh
+        log_n = self.dist.log_prob(pre_tanh).sum(-1)
+        log_da_du = torch.log(1.0 - action ** 2 + self.epsilon).sum(-1)
+        return log_n - log_da_du
 
 
 class PolicyDist(ABC, nn.Module):
@@ -134,6 +191,25 @@ class GaussinanDist(PolicyDist):
         size = x.size(1) // 2
         mean, stddev = x[:, :size], x[:, size:]
         return GaussianPolicy(mean, F.softplus(stddev))
+
+
+class TanhGaussianDist(GaussinanDist):
+    """Tanh clipped Gaussian policy
+    """
+    def __init__(
+            self,
+            action_dim: int,
+            logstd_range: Tuple[float, float] = (-20.0, 2.0),
+            **kwargs,
+    ) -> None:
+        super().__init__(action_dim)
+        self.logstd_range = logstd_range
+
+    def forward(self, x: Tensor) -> Policy:
+        size = x.size(1) // 2
+        mu, logstd = x[:, :size], x[:, size:]
+        std = torch.exp(logstd.clamp_(*self.logstd_range))
+        return TanhGaussianPolicy(mu, std)
 
 
 class SeparateStdGaussianDist(PolicyDist):
