8a081935d23476a9f2327eedb68f4e7995e2f936
kngwyu <yuji.kngw.80s.revive@gmail.com>
2020-05-30 19:49:56 +0900

    Correctly save/load _rms in NormalizeReward/ObsParallel

--- tests/test_save_and_load.py
+++ tests/test_save_and_load.py
@@ -5,9 +5,16 @@ from torch.optim import Adam
 
 from rainy import Config
 from rainy.agents import PPOAgent
-from rainy.envs import MultiProcEnv
+from rainy.envs import MultiProcEnv, PyBullet, pybullet_parallel
 
 LOG_DIR = Path("/tmp/rainy-test/")
+
+try:
+    import pybullet  # noqa
+
+    HAS_PYBULLET = True
+except ImportError:
+    HAS_PYBULLET = False
 
 
 def config() -> Config:
@@ -36,4 +43,22 @@ def test_ppo_save() -> None:
     ppo.load(path.as_posix())
     assert ppo.clip_eps == 0.2
     assert ppo.optimizer.param_groups[0]["lr"] == 1.0
+    ppo.close()
+
+
+@pytest.mark.filterwarnings("ignore:PkgResourcesDeprecationWarning")
+@pytest.mark.skipif(not HAS_PYBULLET, reason="PyBullet is an optional dependency")
+def test_rms_save() -> None:
+    c = config()
+    c.set_env(lambda: PyBullet())
+    c.set_parallel_env(pybullet_parallel())
+    ppo = PPOAgent(c)
+    ppo.penv.as_cls("NormalizeObsParallel")._rms.mean = 10.0
+    ppo.save("ppo-agent.pth")
+    ppo.close()
+    ppo = PPOAgent(c)
+    path = ppo.config.logger.logdir.joinpath("ppo-agent.pth")
+    ppo.load(path.as_posix())
+    mean = ppo.penv.as_cls("NormalizeObsParallel")._rms.mean.mean()
+    assert 9.999 <= mean <= 10.001
     ppo.close()
